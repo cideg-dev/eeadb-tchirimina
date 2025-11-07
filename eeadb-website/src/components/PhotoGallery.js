df4ef8986e25d1a800dc6d84449890d52f5f4d46
cideg-dev <cideg.p.line@gmail.com>
2025-11-07 13:50:25 +0100

    Ajout de la directive 'use client' aux composants utilisant des hooks React

--- eeadb-website/src/components/PhotoGallery.js
+++ eeadb-website/src/components/PhotoGallery.js
@@ -1,3 +1,5 @@
+'use client';
+
 import { useState, useEffect } from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
